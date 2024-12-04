547c5043fcc941b60b5b3af808bfe1c26faf1a4f
Nilesh410 <shirudenileshs@gmail.com>
2024-12-04 16:08:38 +0530

    study datay types in JS

---
+++ 2_Data_Types/1_example.js
@@ -0,0 +1,35 @@
+console.log(typeof(34.2)); //number
+
+console.log(typeof("Nilesh Shirude")) //string
+console.log(typeof('Nilesh Shirude')) //string
+console.log(typeof(`Nilesh Shirude`)) //string
+
+console.log(typeof(true)) //boolean
+
+console.log(typeof(null)) //object
+
+console.log(typeof(undefined)) //undefined
+
+console.log(23.2+2)//25.2
+console.log(true+1) //2
+console.log(false-1) //-1
+console.log(12.1+true) //13.1
+
+console.log(1+"100") //1100
+console.log(typeof(1+"100")) //string
+console.log(+"100")//100
+console.log(typeof(+"100")) //number
+console.log(typeof(2+true)) //number
+
+console.log(2+undefined)//NaN
+console.log(typeof(2+undefined))//number
+console.log(2+null) //2
+console.log(typeof(2+null)) //number
+
+console.log(null+undefined) //NaN
+console.log(typeof(null+undefined)) //number
+console.log(typeof(NaN))//number
+
+console.log("100"+null)//100null
+console.log(undefined+"100")//undefined100
+
