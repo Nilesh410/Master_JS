88d4b79785bc1aa4c1e4eb17273bca0e11c13b18
Nilesh410 <shirudenileshs@gmail.com>
2024-12-05 11:49:37 +0530

    perform operations on string

--- 2_Data_Types/8_operations.js
+++ 2_Data_Types/8_operations.js
@@ -27,6 +27,7 @@ console.log(1+"2"+"2")//122 */
 console.log(1-"2")//-1
 console.log("2"-1)//1
 console.log("2"-1-1)//0
+console.log("2"-"2"-1) //-1
 console.log(1-1-"2")//-2
 console.log(1-"2"-"2")//-3
 
