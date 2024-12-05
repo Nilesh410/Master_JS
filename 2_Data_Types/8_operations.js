cb337e705386bb10ce2c2947db4d6ca000cda41b
Nilesh410 <shirudenileshs@gmail.com>
2024-12-05 11:37:16 +0530

    perform operation on string (number)

---
+++ 2_Data_Types/8_operations.js
@@ -0,0 +1,52 @@
+/* let value=3
+let negatvalue=-value
+console.log(negatvalue) //-3 */
+
+/* 
+console.log(2+2)//4
+console.log(2-2)//0
+console.log(2*2)//4
+console.log(2/2)//1
+console.log(2**2)//4
+console.log(2**3)//8
+console.log(2%2)//0 */
+
+/* let s1="Nilesh"
+let s2=" Shirude"
+let s3=s1+s2
+console.log(s3) */
+
+/* console.log(2+"1")//21
+console.log(1+"2")//12
+console.log("1"+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32 
+console.log(1+"2"+"2")//122 */
+
+//substraction operation with string 
+console.log(1-"2")//-1
+console.log("2"-1)//1
+console.log("2"-1-1)//0
+console.log(1-1-"2")//-2
+console.log(1-"2"-"2")//-3
+
+//multiplication and division
+console.log("2"*2)//4
+console.log(2*"3")//6
+console.log("2"/2)//1
+console.log(6/"2")//3
+
+console.log("2"*0)//0
+console.log("2"/0)//infinity
+
+console.log("2"+"2")//22
+console.log("2"-"2")//0
+console.log("2"*"2")//4
+console.log("2"/"2")//1
+console.log("2"*"Nilesh") //NaN
+
+/* If string is a number then only 
+   string +string=>string
+   string -string=>number
+   string *string=>number 
+   string /string=>number*/
