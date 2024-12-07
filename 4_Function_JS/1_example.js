42500bb8fbf35e0f56cad0037d05a5c3f0e13cbe
Nilesh410 <shirudenileshs@gmail.com>
2024-12-07 23:34:20 +0530

    function hoisting

--- 4_Function_JS/1_example.js
+++ 4_Function_JS/1_example.js
@@ -22,3 +22,6 @@ result("NSS") */
 //4. IIFE Immediate Invoked Function Expression 
 /* (function (name){console.log(`My name is ${name}`)})("Nilesh Shirude") */
 
+//5. Arrow Function 
+let result=(name)=>{console.log(`My name is ${name}`)}
+result("Jyoti")
--- 4_Function_JS/2_example.js
+++ 4_Function_JS/2_example.js
@@ -1,1 +1,19 @@
-funct
+/* Que 1) what is function hoising ? 
+Ans:- calling the function before its declaration and definition and its execute the definition
+Function hoisting is applicable only to function decleration not the function expression properties
+*/
+
+/* display("Nilesh Shirude") //Nilesh Shirude
+function display(name)
+{
+    console.log(`Name is ${name}`)
+} */
+
+/* result("Nilesh") //error for var as well as let.const variable 
+const result=function(name)
+{
+    console.log(`My name is ${name}`)
+} */
+
+myname("NSS")  //same error
+let myname=new Function('name','console.log(`my name is ${name}`)')
