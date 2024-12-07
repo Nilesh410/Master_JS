661faea05f0e8db42a8df6cbe8746e7af2cf2700
Nilesh410 <shirudenileshs@gmail.com>
2024-12-07 23:18:25 +0530

    study different way to write down in JS

---
+++ 4_Function_JS/1_example.js
@@ -0,0 +1,24 @@
+//1.Function Decleration or Function Defintion 
+/* function display(name)
+{
+    console.log(`My name is ${name}`)
+}
+display("Nilesh Shirude") */
+
+//2.Function Expression assign to any variable
+/* let result= function display(name)
+{
+    console.log(`My name is ${name}`)
+}
+result("Nilesh Shiurde") */
+
+//3. Ananymous Function (Function does nto have any name)
+/* let result= function(name)
+{
+   console.log(`My name is ${name}`)
+}
+result("NSS") */
+
+//4. IIFE Immediate Invoked Function Expression 
+/* (function (name){console.log(`My name is ${name}`)})("Nilesh Shirude") */
+
