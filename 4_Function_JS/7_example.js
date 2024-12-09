81aaff857f89ef56eaaddfcfcc2bfb7620d77cdb
Nilesh410 <shirudenileshs@gmail.com>
2024-12-09 17:15:13 +0530

    add example no 7

--- 4_Function_JS/7_example.js
+++ 4_Function_JS/7_example.js
@@ -6,14 +6,14 @@ which is then invoked inside the outer function to complete some kind of routine
 
 let evenCheck=(ele)=>{
     return ele%2 === 0
-}
+}//call back function 
 
 console.log(evenCheck(23))
 
 const array1=[2,44,68,96]
 const array2=[34,21,56,34,7]
 
-const result1=array1.every(evenCheck)
+const result1=array1.every(evenCheck)//higher order function 
 const result2=array2.every(evenCheck)
 console.log("array1:",result1)
 console.log("array2:",result2)
