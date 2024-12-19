bf49def6558dbc4f89e65ce06022b2685e13165d
Nilesh410 <shirudenileshs@gmail.com>
2024-12-19 22:56:10 +0530

    use of ternary operator

---
+++ 10_Condition_Stat/8_switchcase_example.js
@@ -0,0 +1,8 @@
+//Find out the
+const a = 5,
+  b = 8,
+  c = 6
+
+if (a > b && a > c) console.log('a is largest number')
+else if (b > c) console.log('b is largest number')
+else console.log('c is largest number')
---
+++ 10_Condition_Stat/9_ternary_op.js
@@ -0,0 +1,8 @@
+const a = 5,
+  b = 8,
+  c = 6;
+
+  (a>b&&a>c)
+    ? console.log('a is largest no among three')
+    : (b>c) ? console.log('b is largest no')
+    : console.log('c is largest no')
