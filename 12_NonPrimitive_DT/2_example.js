287b39edb4e24e52aeadf186a8b546067c270405
Nilesh410 <shirudenileshs@gmail.com>
2024-12-24 23:40:09 +0530

    const object variable allow to change or update the object'

---
+++ 12_NonPrimitive_DT/2_example.js
@@ -0,0 +1,14 @@
+const obj1={
+    emp_name:'Nilesh Shirude',
+    role:'Assistant Professor',
+    dept:'E&TC',
+    joining_date:'4th Sept 2014',
+    year_exp:10,
+    email_id:'nsshirude@pict.edu',
+    "mob-no":8956210124,
+    skill:'JAVA, JS, OOP Programming, DSA'
+
+}
+console.log(obj1)
+obj1.edu='MTech'
+console.log(obj1)
