12f10634f927aee9ce3ebbcaf1f9d08af7b4d8f6
Nilesh410 <shirudenileshs@gmail.com>
2024-12-31 00:56:16 +0530

    add example

---
+++ 9_Operator/7_example.js
@@ -0,0 +1,4 @@
+const a="Nilesh Shirude"
+const b="Shirude Nilesh"
+const result=a&&b
+console.log(result)
---
+++ Practice_Example/1_example.js
@@ -0,0 +1,12 @@
+var a='Hello JS'
+function show()
+{
+    a='Welcome in Learning'
+    return 
+    function a(){
+        a='JS is prog language'
+    }
+   a()
+}
+show()
+console.log(a);
