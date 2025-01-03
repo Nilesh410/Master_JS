f632bc4f93d030465554602a4a4f6d98bd4fcb71
Nilesh410 <shirudenileshs@gmail.com>
2025-01-03 23:37:04 +0530

    examples

---
+++ Practice_Example/3_example.js
@@ -0,0 +1,8 @@
+const arr=[2,5,6,3,7]
+console.log(arr)
+
+let new_arr=arr.map((ele)=>{
+    ele+2
+})
+
+console.log(new_arr);
---
+++ Practice_Example/4_example.js
@@ -0,0 +1,14 @@
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+  ];
+
+  let result=students.filter(items =>items.grade>=85 
+  )
+  console.log(result)
+const numbers = [1, 2, 3, 4];
+const evens = numbers.filter(item => item % 2 === 0);
+console.log(evens); // [2, 4]
