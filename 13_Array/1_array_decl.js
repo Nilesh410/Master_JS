a4f8b9b75f22fb3bce429d1f7eda4709eaa2885c
Nilesh410 <shirudenileshs@gmail.com>
2025-01-11 11:55:43 +0530

    Definition of array

---
+++ 13_Array/1_array_decl.js
@@ -0,0 +1,7 @@
+//Array:- In JS array is a data strcture to store the heterogenous data at index based
+//Syntax :- variable decleration [array elements seperated by comma]
+//Array is one of the non primitve data type and its type its nothing but the object 
+//Object have object literal, array, and function non primitive data type 
+//when you find out the typeof(arrayname)=> object 
+//typeof(object_literal)=>object 
+//typeof(function_name)=>function 
