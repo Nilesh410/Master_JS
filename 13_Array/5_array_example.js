7154666b90d1f8242c02327714b33cdaa42d8b56
Nilesh410 <shirudenileshs@gmail.com>
2025-01-18 23:58:48 +0530

    update all pending file

---
+++ 13_Array/5_array_example.js
@@ -0,0 +1,7 @@
+const event1Participants = ["Alice", "Bob", "Charlie"];
+
+const event2Participants = ["David", "Eve", "Frank"];
+console.log(event1Participants.concat(event2Participants))
+
+const weeklyTemperatures = [72, 75, 79, 83, 78, 74, 70];
+console.log(weeklyTemperatures.slice(1,4))
---
+++ 13_Array/6_multidim.js
@@ -0,0 +1,3 @@
+let final_array=[["x",null,null],[null,null,"O"],["O",null,null]]
+console.log(final_array)
+console.log(final_array[2][0]);
