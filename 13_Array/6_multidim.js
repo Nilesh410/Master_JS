2fa213fb509636b414385fa80272d2a6b58145e1
Nilesh410 <shirudenileshs@gmail.com>
2025-01-20 22:29:16 +0530

    solve example on multidim

--- 13_Array/6_multidim.js
+++ 13_Array/6_multidim.js
@@ -1,3 +1,13 @@
 let final_array=[["x",null,null],[null,null,"O"],["O",null,null]]
 console.log(final_array)
 console.log(final_array[2][0]);
+
+const nestedArray=[
+    [[1,2],3,4],
+    [5,[6,7],8],
+    [9,10,[11,12]]
+]
+
+console.log(nestedArray[0][0])
+console.log(nestedArray[1][1])
+console.log(nestedArray[2][2])
