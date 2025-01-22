3fa44ff81d0580ef69e0dba372b87c243b5c2ce4
Nilesh410 <shirudenileshs@gmail.com>
2025-01-22 23:52:34 +0530

    practice example

---
+++ Practice_Example/12_example.js
@@ -0,0 +1,12 @@
+let a=10
+let b="12"
+let c="PICT"
+console.log(a+b);//1012
+console.log(typeof(a+b))//string
+console.log(a-b)//-2
+console.log(typeof(a-b))//number
+console.log(a-c)//NaN
+console.log(typeof(a-c))//number
+console.log(a/b)//0.8333
+console.log(true+"12")
+console.log(true-"12")
