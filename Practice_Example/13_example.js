0e4fffd67d34734bd0b49e41bd845dbed8e53a0c
Nilesh410 <shirudenileshs@gmail.com>
2025-01-24 00:12:27 +0530

    decl let and var after function calling

--- Practice_Example/13_example.js
+++ Practice_Example/13_example.js
@@ -1,6 +1,19 @@
-let a=10
+/* 
 function display()
 {
     console.log(a)
+    a=20
+    console.log(a)
 }
+
+display()
+let a=10 */
+//After declaration and initialization of a let variable shows error to access in function but not for var type
+
+function display() {
+  console.log(a)
+  a = 20
+  console.log(a)
+}
+var a
 display()
---
+++ Practice_Example/14_example.js
@@ -0,0 +1,10 @@
+function display() {
+    //console.log(a)//undefined
+   // a = 20
+    //console.log(a)//20
+  }
+
+  display()
+ let a
+  console.log(a)
+  
