b8792cd66b089f8df5e5bfa91f1a58a833aeeb5b
Nilesh410 <shirudenileshs@gmail.com>
2025-01-26 00:14:33 +0530

    use of substring fucntion on given string

---
+++ 8_String/4_example.js
@@ -0,0 +1,8 @@
+let str="26 Jan is India's Republic Day"
+let str1="Welcome in India"
+console.log(str.replaceAll(" ",""))//replace multiple occurence
+console.log(str1.replace(" ","")) //replace only first occurence 
+let str2="26 Jan is India's Republic Day"
+let newstr=parseInt(str2.substring(0,2))
+console.log(newstr)
+console.log(typeof(newstr))
