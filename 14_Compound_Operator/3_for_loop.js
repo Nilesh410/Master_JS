4a6f357110920686412d8c2a3ddc9ad5c7477aa9
Nilesh410 <shirudenileshs@gmail.com>
2025-02-10 14:26:53 +0530

    use of for loop

---
+++ 14_Compound_Operator/3_for_loop.js
@@ -0,0 +1,14 @@
+console.log("use of for loop")
+//const a=10
+let a=5
+// for(let i=0;i<a;i++)
+// {
+//     console.log(i*2)
+// }
+for(var i=0;i<a;i++)
+{
+    console.log(i*2)
+}
+console.log(i)
+console.log(i++)
+console.log(i)
