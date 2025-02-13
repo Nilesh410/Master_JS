a4732fcbd2621e698ac4dadc3ce82a3c187b2b29
Nilesh410 <shirudenileshs@gmail.com>
2025-02-13 14:33:32 +0530

    prime no logic

---
+++ 14_Compound_Operator/11_assginment_no7.js
@@ -0,0 +1,26 @@
+//Write JavaScript code that checks whether a given number is a prime number or not. Print "Prime" if it's a prime number, otherwise print "Not Prime".
+let num=5
+let alert=0
+if(num===1||num===0)
+{
+    console.log("0 and 1 is not a prime no");
+}
+else{
+    for(i=2;i<num/2;i++)
+    {
+        if(num%i===0)
+        {
+            alert=1
+            break
+        }
+        
+    }
+    if(alert===0)
+        console.log("given no is a prime number")
+    else
+        console.log("given no is not a prime number")
+
+}
+
+
+
