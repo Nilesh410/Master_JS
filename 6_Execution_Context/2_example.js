98bd63cff5e0053ea99b188db40af7aa42d1bf43
Nilesh410 <shirudenileshs@gmail.com>
2025-03-11 13:54:38 +0530

    global and local execuion context

---
+++ 6_Execution_Context/2_example.js
@@ -0,0 +1,16 @@
+
+debugger
+show()
+const userName="Nilesh Shirude"
+const userAge=35
+
+function show()
+{
+    console.log("I am a programmer");
+    programming("JAVA Script")
+}
+
+function programming(language)
+{
+    return `I am master in ${language}`
+}
