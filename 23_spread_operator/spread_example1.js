9978eac1855c8612e1be1bf6a82a77bed0ef7fd6
Nilesh410 <shirudenileshs@gmail.com>
2025-04-19 00:11:42 +0530

    add use of spread operator

---
+++ 23_spread_operator/spread_example1.js
@@ -0,0 +1,7 @@
+// Use of spread operator to concat two areay as well as object 
+
+let first_arr=[1,2,3,4,5]
+let second_arr=[7,8,9,10]
+
+const new_arr=[...first_arr,...second_arr]
+console.log(new_arr);
