42e84a803e2b814d5c2e180028556191ba00dd59
Nilesh410 <shirudenileshs@gmail.com>
2025-04-19 00:02:00 +0530

    example for the use of arguments keyword

--- 22_argument_default/example1.js
+++ 22_argument_default/example1.js
@@ -1,6 +1,16 @@
-function demo ()
+/* function demo ()
 {
     console.log(arguments)
-}
+} */
 
-demo(2,3,4)
+//Find out the sume of arguments value but we dont know how many arguments we passed
+function demo()
+{
+    let sum=0
+    for( let i=0;i<arguments.length;i++)
+    {
+        sum +=arguments[i]
+    }
+    return sum
+}
+console.log(demo(2,3,4))
